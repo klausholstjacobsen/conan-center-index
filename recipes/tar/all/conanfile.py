a18b104caee91e280d94cf9781014b5a108aab68
klausholstjacobsen <klaus.holst.jacobsen@gmail.com>
2023-07-23 14:15:37 +0200

    Updated to conan 2.0

--- recipes/tar/all/conanfile.py
+++ recipes/tar/all/conanfile.py
@@ -1,8 +1,11 @@
-from conans import AutoToolsBuildEnvironment, ConanFile, tools
-from conans.errors import ConanInvalidConfiguration
+from conan import ConanFile
+from conan.errors import ConanInvalidConfiguration
+from conan.tools.gnu import Autotools, AutotoolsToolchain, AutotoolsDeps, PkgConfigDeps
+from conan.tools.files import collect_libs, copy, get, load, replace_in_file, rmdir, save, patch, export_conandata_patches
+from conan.tools.layout import basic_layout
 import os
 
-required_conan_version = ">=1.33.0"
+required_conan_version = ">=1.60.0"
 
 
 class TarConan(ConanFile):
@@ -17,21 +20,18 @@ class TarConan(ConanFile):
     _autotools = None
 
     @property
-    def _source_subfolder(self):
-        return "source_subfolder"
-
-    @property
     def _settings_build(self):
         return getattr(self, "settings_build", self.settings)
 
     def configure(self):
-        del self.settings.compiler.libcxx
-        del self.settings.compiler.cppstd
+        self.settings.rm_safe("compiler.libcxx")
+        self.settings.rm_safe("compiler.cppstd")
 
     def requirements(self):
         self.requires("bzip2/1.0.8")
         self.requires("lzip/1.21")
         self.requires("xz_utils/5.2.5")
+        self.requires("zstd/1.5.2")
 
     def validate(self):
         if self.settings.os == "Windows":
@@ -43,18 +43,41 @@ class TarConan(ConanFile):
         del self.info.settings.compiler
 
     def source(self):
-        tools.get(**self.conan_data["sources"][self.version],
-                  destination=self._source_subfolder, strip_root=True)
+        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+
+    def generate(self):
+        ad = AutotoolsDeps(self)
+        ad.generate()
+
+        tc = AutotoolsToolchain(self)
+        if 'Neutrino' in self.settings.os:
+            tc.extra_ldflags = ["-lsocket"]
+        tc.generate()
+
+    def _patch_sources(self):
+        print("patching for {}-{}-{}".format(self.version, str(self.settings.os).lower(), self.settings.get_safe("os.version")))
+
+        # general patches
+        for p in self.conan_data["patches"].get(self.version, []):
+            print(f"patching general files: {p['patch_file']}")
+            patch(self, **p, base_path=self.source_folder)
+        
+        # os specific patches
+        for p in self.conan_data["patches"].get("{}-{}".format(self.version, str(self.settings.os).lower()), []):
+            print(f"patching os specific files: {p['patch_file']}")
+            patch(self, **p, base_path=self.source_folder)
+
+        # os version specific patches
+        for p in self.conan_data["patches"].get("{}-{}-{}".format(self.version, str(self.settings.os).lower(), self.settings.get_safe("os.version")), []):
+            print(f"patching os version specific: {p['patch_file']}")
+            patch(self, **p, base_path=self.source_folder)
 
     def _configure_autotools(self):
-        if self._autotools:
-            return self._autotools
-        self._autotools = AutoToolsBuildEnvironment(self)
-        self._autotools.libs = []
         bzip2_exe = "bzip2"  # FIXME: get from bzip2 recipe
         lzip_exe = "lzip"  # FIXME: get from lzip recipe
         lzma_exe = "lzma"  # FIXME: get from xz_utils recipe
         xz_exe = "xz"  # FIXME: get from xz_utils recipe
+        zstd_exe = "zstd"  # FIXME: get from xz_utils recipe
         args = [
             "--disable-acl",
             "--disable-nls",
@@ -67,26 +90,30 @@ class TarConan(ConanFile):
             "--with-lzma={}".format(lzma_exe),
             # "--without-lzop",  # FIXME: this will use sytem lzop
             "--with-xz={}".format(xz_exe),
-            # "--without-zstd",  # FIXME: this will use system zstd (current zstd recipe does not build programs)
+            "--with-zstd={}".format(zstd_exe),
         ]
-        self._autotools.configure(args=args, configure_dir=self._source_subfolder)
-        return self._autotools
+        return args
+
 
     def build(self):
-        for patch in self.conan_data.get("patches", {}).get(self.version, []):
-            tools.patch(**patch)
+        self._patch_sources()
+
         if self.settings.compiler == "Visual Studio":
             tools.replace_in_file(os.path.join(self._source_subfolder, "gnu", "faccessat.c"),
                                   "_GL_INCLUDING_UNISTD_H", "_GL_INCLUDING_UNISTD_H_NOP")
-        autotools = self._configure_autotools()
+
+        self._patch_sources()
+
+        autotools = Autotools(self)
+        autotools.configure(args=self._configure_args)
         autotools.make()
 
     def package(self):
-        self.copy("COPYING", src=self._source_subfolder, dst="licenses")
-        autotools = self._configure_autotools()
+        copy(self, "COPYING", src=self.source_folder, dst=os.path.join(self.package_folder, "licenses"))
+        autotools = Autotools(self)
         autotools.install()
 
-        tools.rmdir(os.path.join(self.package_folder, "share"))
+        rmdir(self, os.path.join(self.package_folder, "share"))
 
     def package_info(self):
         bin_path = os.path.join(self.package_folder, "bin")
